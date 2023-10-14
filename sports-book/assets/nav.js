89d4160873edb35f014a788841f89c1fb2773648
developweb <developmentweb10@gmail.com>
2023-10-14 12:12:21 +0530

    updated files

--- sports-book/assets/nav.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector("button.mobile-menu-button");
-        const menu = document.querySelector(".mobile-menu");
-
-        btn.addEventListener("click", () => {
-            alert("Book")
-            menu.classList.toggle("hidden");
-        });
